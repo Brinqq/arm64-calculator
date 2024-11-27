536dcc42dc89b4e591179c679ffb0c7c93e3bf4e
brinq <andrewrdavis18@gmail.com>
2024-11-27 03:24:13 -0800

    change get user input function to take in pointers to buffer and returned length

--- src/input.s
+++ src/input.s
@@ -6,44 +6,46 @@
 
 .text
 
-// right now user iput has a fixed size of {MAX_USER_INPUT_S} chars in static memory
-.equ MAX_USER_INPUT_S, 32
 //TODO: maybe allocate dynamically
+//TODO: if user inputed string is longer than max count passed to get input function program will cease to function. But dont know how to fix rn so....
 
-// char* func()
+// int func(char*, &length, max_count)
 _input_get_user_input:
-  mov x4, x30
+  mov x3, x0
+  mov x4, x1
   mov x0, #0
-  adrp x1, user_input_buffer@PAGE
-  add x1, x1, user_input_buffer@PAGEOFF
-  mov x6, x1
-  mov x2, MAX_USER_INPUT_S
+  mov x1, x3
   mov x16, SYS_READ
   svc 0
-  mov x5, #0
-  cmp x0, x5
+  str x0, [x4]
+  mov x3, #0
+  cmp x0, x3
+  b.eq read_error
   cmp x0, x2
-  b.gt max_size_exceeded_error 
-  b.ne read_no_error
-  mov x0, #1
+  b.gt max_chars_exceeded_error
+  ret
+  read_error:
+  mov x5, x30
   adrp x0, user_input_error_read@PAGE
-  add x0, x0 , user_input_error_read@PAGEOFF
+  add x0, x0, user_input_error_read@PAGEOFF 
   mov x1, #31
   bl _utils_print_charbuffer
-  mov x6, x30
+  mov x0, #1
+  mov x30, x5
   ret
-  max_size_exceeded_error:
-  mov x0, #-1
-  ret
-  read_no_error:
-  mov x1, x0
-  mov x0, x6
+  max_chars_exceeded_error:
+  mov x5, x30
+  adrp x0, user_input_mce_error@PAGE
+  add x0, x0, user_input_mce_error@PAGEOFF 
+  mov x1, #58
   bl _utils_print_charbuffer
-  mov x30, x4
+  mov x0, #1
+  mov x30, x5
   ret
 
 .data
 user_input_error_read: .ascii "Error while reading user input!\n"
+user_input_mce_error: .ascii "Maximum characters allowed exceeded by user inputed string\n"
 
 .bss
 user_input_buffer: .skip MAX_USER_INPUT_S
--- src/program.s
+++ src/program.s
@@ -2,6 +2,8 @@
 .include "input.s"
 .include "utils.s"
 
+.equ MAX_USER_BYTES, 0x10
+
 .text
 .global _main
 .align 4
@@ -11,13 +13,37 @@ _exit:
   mov x16, SYS_EXIT
   svc 0
 
+// struct users_input_t{
+//  pBuffer: 8
+//  char_buffer_legth: 4
+//}
+
+_executable_loop:
+  mov x7, x30
+  sub sp, sp, #32
+  mov x0, sp
+  mov x1, sp
+  add x1, x1, MAX_USER_BYTES 
+  mov x2, MAX_USER_BYTES
+  bl _input_get_user_input1
+  mov x0, sp
+  ldr x1, [sp, 0x10]
+  bl _utils_print_charbuffer
+  add sp, sp, #32
+  mov x30, x7
+  ret
+
+  
+   
 
 _main:
   mov x7, #2
-  bl _input_get_user_input
+  bl _executable_loop
   bl _exit
 
+.bss
 .data
+ts: .ascii "hello world\n"
 
 
 
